d63eb61e2e12ce6d1d77a8deed3fff387539fa2b
dee098 <50029262+dee098@users.noreply.github.com>
2023-10-19 17:41:02 +0000

    fix models

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-python3 -m venv .venv
-git clone https://github.com/dee098/printdb.git
+if [ ! -d "printdb" ]; then
+    git clone https://github.com/dee098/printdb.git
+else
+    echo  printdb exist
+fi
+
+if [ ! -d ".venv" ]; then
+    python3 -m venv .venv
+else
+    echo .venv exist
+fi
 . .venv/bin/activate
 cd printdb
 pip install -r ./requirements.txt
+read -p "Press enter to continue"
+
 python3 app.py
